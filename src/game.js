0b2ad6b9b815ec4940d8df5b6e7cb40701aa62ce
carsonbarlow <carson@mediaforge.com>
2013-08-24 23:01:17 -0600

    added Game.player variables

--- src/game.js
+++ src/game.js
@@ -26,17 +26,17 @@ window.onload = function(e){
   Game.input.mouse.mouse_down = false;
   Game.input.keyboard.id_to_key = {'U+0041':'a', 'U+0053':'s', 'U+0044':'d', 'U+0057':'w', 'U+0020':'space'};
 
-
+  // graphics
   Game._time = (new Date).getTime();
-
   Game.graphics = {};
   Game.graphics.canvas = document.getElementById(Game.config.canvas_id);
   Game.graphics.context = Game.graphics.canvas.getContext('2d');
 
-  Game.graphics.canvas.addEventListener('mousedown',function(event){
+  // input events
+  window.addEventListener('mousedown',function(event){
     Game.input.mouse.mouse_down = true;
   });
-  Game.graphics.canvas.addEventListener('mouseup', function(event){
+  window.addEventListener('mouseup', function(event){
     Game.input.mouse.mouse_down = false;
   });
   Game.graphics.canvas.addEventListener('mousemove', function(event){
@@ -46,12 +46,38 @@ window.onload = function(e){
   window.addEventListener('keydown',function(event){
     // console.log(event.keyIdentifier);
     Game.input.keyboard[Game.input.keyboard.id_to_key[event.keyIdentifier]] = true;
-
   });
   window.addEventListener('keyup',function(event){
     Game.input.keyboard[Game.input.keyboard.id_to_key[event.keyIdentifier]] = false;
   });
 
+  // player
+  Game.player = {
+    luck: 100,
+    health: 10,
+    health_regen: 1,
+    speed: 50,
+    melee: {
+      damage: 4,
+      reach: 10,
+      cooldown: 500,
+      cooldown_left: 0
+    },
+    ranged: {
+      damage: 3,
+      fire_rate: 400,
+      ammo: 8,
+      max_ammo: 8,
+      reload_time: 5000,
+      reload_time_left: 0
+    },
+    bomb: {
+      damage: 10,
+      range: 25,
+      cooldown: 10000,
+      cooldown_left: 0
+    }
+  }
 
   // GAME LOOP
   Game.game_loop = setInterval(function(){
@@ -64,7 +90,7 @@ window.onload = function(e){
 
   var tgo = {};
   Game.update = function(){
-    if (Game.input.keyboard.a){console.log(Game.input.mouse.x);}
+    if (Game.input.keyboard.a){console.log(Game.player.health);}
     // initializing test rectangle
     if (typeof tgo.test_rect == 'undefined'){
       tgo.test_rect = {};
